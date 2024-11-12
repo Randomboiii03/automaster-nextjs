ed1e15c69f76c54122e6089e8cb82c05d02e4616
Randomboiii03 <dandyramos03@gmail.com>
2024-11-12 17:20:57 +0800

    add markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,26 @@
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Theme } from '@radix-ui/themes';
 import NavBar from "./NavBar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -29,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <Theme>
           <NavBar />
--- app/modules/new/page.tsx
+++ app/modules/new/page.tsx
@@ -1,13 +1,14 @@
 'use client';
-import React from 'react'
 import { TextField, TextArea, Button } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewModulePage = () => {
   return (
     <div className='max-w-xl space-y-4'>
         <TextField.Root placeholder='Title'>
         </TextField.Root>
-        <TextArea placeholder="Write description..." />
+        <SimpleMDE placeholder="Write description..." />
         <Button>Create New Issue</Button>
     </div>
   )
